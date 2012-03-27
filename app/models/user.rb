526a00c81902fcf5b1098792cb5c58d7ca7ec344
Katie Soto <KReneSoto@gmail.com>
2012-03-27 13:37:47 -0700

    Added User model, view, controller and resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email #makes only name and email editable from the outside
 
+email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
   validates :email, :presence   => true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  #get "users/new"
+
+  resources :users #allows for REST-style users URL (users/1 goes to users#show)
 
   match '/signup',  :to => 'users#new'
   
